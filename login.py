062dfc0e410f2106080659a6fed0b2bb24514f23
maksimtall <98038173+maksimtall@users.noreply.github.com>
2023-11-01 12:43:52 +0100

    Add files via upload

---
+++ login.py
@@ -0,0 +1,37 @@
+from dbUse import *
+from inout import *
+
+run = 1
+# from inout import ask5
+
+# ask()
+# download()
+# readall()
+
+# file = openFile("db.json", "r")
+
+# listAll(file)
+# jsonDic = getUserObj(file)
+# print(jsonDic)
+
+
+def LoginLoop():
+    askObj = ask()
+    if askObj:
+        if askObj.get("error") is not None:
+            print(askObj.get("error"))
+            LoginLoop()
+        elif askObj.get("exit") is not None:
+            print(askObj.get("exit"))
+        elif askObj.get("signup") is not None:
+            WriteUser(askObj)
+            LoginLoop()
+        else:
+            print("Hello ", askObj.get("name"))
+
+
+LoginLoop()
+
+# ask()
+# close(file)
+# overwrite(file)
